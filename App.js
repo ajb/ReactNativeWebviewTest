703c2c218c27203f00a24b9313aa159cf1213612
Adam Becker <ad@mbecker.cc>
2020-06-27 21:55:17 -0700

    make it work!

--- App.js
+++ App.js
@@ -1,23 +1,102 @@
-import React, { useRef } from 'react';
+const BOARDCAT_SERVICE_ID = '19b10000-e8f2-537e-4f6c-d104768a1214'
+const BOARDCAT_CHARACTERISTIC_ID = '19b10001-e8f2-537e-4f6c-d104768a1214'
+
+import React, { useRef, useState } from 'react';
 import {
   View,
   Text,
 } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { BleManager } from 'react-native-ble-plx';
+import { encode } from 'base-64';
+
+const manager = new BleManager()
+
+function pad(n, width, z) {
+  z = z || '0';
+  n = n + '';
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
 
 export default function App() {
   const webviewRef = useRef()
+  const [connected, setConnected] = useState(false)
+  const [connectedDevice, setConnectedDevice] = useState(null)
 
-  // setTimeout(() => {
-  //   webviewRef.current.injectJavaScript("window.ReactNativeWebViewMessage = 'lol';")
-  // }, 1000)
+  function stopScanning() {
+    manager.stopDeviceScan()
+  }
+
+  function connect() {
+    if (connected) return;
+    sendCallback('onConnecting')
+    setTimeout(stopScanning, 5000)
+
+    manager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.log('Error: ', error);
+        return
+      }
+
+      if (device.name === 'BoardCat LED') {
+        stopScanning()
+
+        device.connect().then((device) => {
+          return device.discoverAllServicesAndCharacteristics()
+        }).then((device) => {
+          setConnected(true)
+          setConnectedDevice(device)
+          sendCallback('onSuccess')
+
+          device.onDisconnected(() => {
+            sendCallback('onDisconnect')
+            setConnected(false)
+            setConnectedDevice(null)
+          })
+        }).catch((error) => {
+          console.log('Error: ', error.message)
+        })
+       }
+    });
+  }
+
+  function disconnect() {
+    if (!connectedDevice) return;
+    connectedDevice.cancelConnection()
+    setConnected(false)
+    setConnectedDevice(null)
+    sendCallback('onDisconnect')
+  }
+
+  function write(str) {
+    if (!connectedDevice) return;
+    connectedDevice.writeCharacteristicWithResponseForService(
+      BOARDCAT_SERVICE_ID,
+      BOARDCAT_CHARACTERISTIC_ID,
+      encode(str)
+    )
+  }
+
+  function sendCallback(cbName) {
+    webviewRef.current.injectJavaScript("window.ReactNativeBTCallbacks && window.ReactNativeBTCallbacks['" + cbName + "']()")
+  }
 
   function handleMessage(message) {
     let parsedMessage = JSON.parse(message)
 
     switch(parsedMessage.type) {
-      case 'CONNECT_BT':
-      alert('connect bt called')
+      case 'connect':
+      connect()
+      break
+
+      case 'disconnect':
+      disconnect()
+      break
+
+      case 'write':
+      write(parsedMessage.payload)
+      break
+
       return
     }
   }
@@ -25,7 +104,7 @@ export default function App() {
   return (
     <WebView
       ref={webviewRef}
-      source={{ uri: 'http://172.20.10.3:3000' }}
+      source={{ uri: 'https://app.boardc.at' }}
       onMessage={(e) => handleMessage(e.nativeEvent.data)}
     />
   )
---
+++ ios/EmptyFile.swift
@@ -0,0 +1,8 @@
+//
+//  EmptyFile.swift
+//  ReactNativeWebviewTest
+//
+//  Created by Adam Becker on 6/27/20.
+//
+
+import Foundation
